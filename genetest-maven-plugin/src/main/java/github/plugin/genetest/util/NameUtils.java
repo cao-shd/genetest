dae3614915ee4d3ad5540ba728fbe81eb62b5d38
caoshd <cao-shd@outlook.com>
2023-10-28 11:22:18 +0800

    [修改] 大写下划线命名法转换BUG

--- genetest-maven-plugin/src/main/java/github/plugin/genetest/util/NameUtils.java
+++ genetest-maven-plugin/src/main/java/github/plugin/genetest/util/NameUtils.java
@@ -4,14 +4,31 @@ public class NameUtils {
     public static String toUnderscoreCase(String camelCase) {
         StringBuilder underscoreCase = new StringBuilder();
         for (int i = 0; i < camelCase.length(); i++) {
-            char c = camelCase.charAt(i);
-            if (Character.isUpperCase(c)) {
-                underscoreCase.append("_").append(Character.toLowerCase(c));
+            char cur = camelCase.charAt(i);
+            char pre;
+            if (i != 0) {
+                pre = camelCase.charAt(i - 1);
             } else {
-                underscoreCase.append(c);
+                pre = 'A';
+            }
+
+            if (Character.isUpperCase(cur)) {
+                if (Character.isUpperCase(pre) || pre == '_') {
+                    underscoreCase.append(Character.toLowerCase(cur));
+                } else {
+                    underscoreCase.append("_").append(Character.toLowerCase(cur));
+                }
+
+            } else {
+                underscoreCase.append(cur);
             }
         }
         return underscoreCase.toString();
+    }
+
+    public static void main(String[] args) {
+        String expression = toUnderscoreCase("MODE_OVERWRITE.equals(mode)");
+        System.out.println(expression);
     }
 
     public static String toCamelCase(String pascalCase) {
