61543ac5119d3b68bca7993be262bb3e9c10ca50
caoshd <cao-shd@outlook.com>
2023-10-29 10:32:20 +0800

    [优化] 修改注释 方法名

--- genetest-maven-plugin/src/main/java/github/plugin/genetest/tool/GeneTool.java
+++ genetest-maven-plugin/src/main/java/github/plugin/genetest/tool/GeneTool.java
@@ -181,7 +181,7 @@ public class GeneTool {
                     createField(srcUnit, testClass, beTestFieldType, beTestFieldName);
                 } else {
                     String annotationName = "org.mockito.InjectMocks";
-                    createMockField(srcUnit, testClass, beTestFieldType, beTestFieldName, annotationName);
+                    createMockedField(srcUnit, testClass, beTestFieldType, beTestFieldName, annotationName);
                 }
             }
         }
@@ -194,7 +194,7 @@ public class GeneTool {
                     createField(srcUnit, testClass, injectFieldType, injectFieldName);
                 } else {
                     String annotationName = "org.mockito.Mock";
-                    createMockField(srcUnit, testClass, injectFieldType, injectFieldName, annotationName);
+                    createMockedField(srcUnit, testClass, injectFieldType, injectFieldName, annotationName);
                 }
             }
         });
@@ -728,7 +728,7 @@ public class GeneTool {
         debug(field);
     }
 
-    private void createMockField(
+    private void createMockedField(
         CompilationUnit srcUnit,
         ClassOrInterfaceDeclaration testClass,
         ClassOrInterfaceType fieldType,
@@ -746,7 +746,6 @@ public class GeneTool {
         // add mock annotation
         AnnotationExpr annotation = AstUtils.createAnnotationExpr(testClass, annotationName);
         AstUtils.addAnnotation(field, annotation);
-        info("create field annotation: " + annotationName);
 
         // print log
         info("create field: " + fieldName);
